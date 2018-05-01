b8476dc89c4e8ffdbcf72d9fe4ca17a6827000a9
Daniel Flor <flor_1337@hotmail.com>
2018-05-01 14:00:51 +0200

    First edition of WoW Api testing.

---
+++ WoWAPITest/WoWAPITest/RootElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoWAPITest
+{
+    public class RootElement
+    {
+        public long lastModified { get; set; }
+        public string name { get; set; }
+        public string realm { get; set; }
+        public string battlegroup { get; set; }
+        public int @class { get; set; }
+        public int race { get; set; }
+        public int gender { get; set; }
+        public int level { get; set; }
+        public int achievementPoints { get; set; }
+        public string thumbnail { get; set; }
+        public string calcClass { get; set; }
+        public int faction { get; set; }
+        public int totalHonorableKills { get; set; }
+    }
+}
