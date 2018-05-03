0d7464c73834d858fc0e5630ce68abc75f343416
Daniel Flor <flor_1337@hotmail.com>
2018-05-03 13:38:28 +0200

    Resigned the form to take Guild & Realm from textbox input

--- WoWAPITest/WoWAPITest/Form1.Designer.cs
+++ WoWAPITest/WoWAPITest/Form1.Designer.cs
@@ -42,6 +42,10 @@
             this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.characterBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.guildBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.refreshTable = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.characterBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.guildBindingSource)).BeginInit();
@@ -49,9 +53,10 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(394, 284);
+            this.textBox1.Location = new System.Drawing.Point(63, 66);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 2;
             // 
             // dataGridView1
@@ -69,9 +74,10 @@
             this.dataGridViewTextBoxColumn5,
             this.dataGridViewTextBoxColumn6});
             this.dataGridView1.DataSource = this.characterBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(93, 43);
+            this.dataGridView1.Location = new System.Drawing.Point(203, 13);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(965, 235);
+            this.dataGridView1.Size = new System.Drawing.Size(943, 289);
             this.dataGridView1.TabIndex = 3;
             // 
             // nameDataGridViewTextBoxColumn
@@ -136,13 +142,54 @@
             // 
             this.guildBindingSource.DataSource = typeof(WoWAPITest.Guild);
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(63, 96);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // refreshTable
+            // 
+            this.refreshTable.Location = new System.Drawing.Point(63, 125);
+            this.refreshTable.Name = "refreshTable";
+            this.refreshTable.Size = new System.Drawing.Size(132, 23);
+            this.refreshTable.TabIndex = 5;
+            this.refreshTable.Text = "Refresh";
+            this.refreshTable.UseVisualStyleBackColor = true;
+            this.refreshTable.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Realm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Guild";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1246, 450);
+            this.ClientSize = new System.Drawing.Size(1661, 554);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.refreshTable);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -173,5 +220,9 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button refreshTable;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- WoWAPITest/WoWAPITest/Form1.cs
+++ WoWAPITest/WoWAPITest/Form1.cs
@@ -16,32 +16,31 @@ namespace WoWAPITest
         public Form1()
         {
             InitializeComponent();
-            CharacterInfo();
         }
 
         public void CharacterInfo()
         {
+            
             RestClient client = new RestClient("https://eu.api.battle.net/");
-            RestRequest request = new RestRequest("wow/guild/Draenor/Pieces?fields=members&locale=en_GB&apikey=jypv5advxjdchn257gup5nqesf777gf9", Method.GET);
+            RestRequest request = new RestRequest("wow/guild/{Realm}/{Guild}?fields=members&locale=en_GB&apikey=jypv5advxjdchn257gup5nqesf777gf9", Method.GET);
+            request.AddUrlSegment("Realm", textBox1.Text);
+            request.AddUrlSegment("Guild", textBox2.Text);
 
-            IRestResponse<Guild> response = client.Execute<Guild>(request);
-            Guild g = response.Data;
+            IRestResponse<Guild> response = client.Execute<Guild>(request);    
 
             List<Character> characters = new List<Character>();
-            foreach (Member m in g.members)
+            foreach (Member m in response.Data.members)
             {
                 characters.Add(m.character);
             }
 
             dataGridView1.DataSource = characters;
-         
-
-
-            //dataGridView2.DataSource = g.members.;
-            // textBox1.Text = response.Data.GuildName;
 
         }
 
-
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CharacterInfo();
+        }
     }
 }
